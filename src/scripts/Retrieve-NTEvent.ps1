be3ba9109b6e27e5dfa7a6e4fc6c6879c2c4ea10
tiggerbob <tiggerbob50@gmail.com>
2022-11-26 23:25:54 -0600

    Fix PS1 scripting error

--- src/scripts/Retrieve-NTEvent.ps1
+++ src/scripts/Retrieve-NTEvent.ps1
@@ -4,13 +4,13 @@ $text = $text.Split($separator)
 $lineLength = 72
 
 For ($i = 0; $i -lt [math]::floor($text.Length/$lineLength); $i++) {
-    $timeParts = $text[2 + 72*$i].Trim() -split " ", 4
+    $timeParts = $text[2 + $lineLength*$i].Trim() -split " ", 4
     $dateTime = $timeParts[0] + " " + $timeParts[1] + " " + $timeParts[2]
 
-    $file = $text[24 + 72*$i].Trim() -split ':', 2
+    $file = $text[24 + $lineLength*$i].Trim() -split ':', 2
     $file = $file[1].Trim()
 
-    $process = $text[36 + 72*$i].Trim() -split ':', 2
+    $process = $text[36 + $lineLength*$i].Trim() -split ':', 2
     $process = $process[1].Trim()
 
     echo $dateTime, $file, $process
